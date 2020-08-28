b776e0d73f6f400d34267d43aa4d2cac7b00882f
Sekhre <66048477+Sekhre@users.noreply.github.com>
2020-08-28 16:19:52 +0530

    Update stickers.py

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -73,7 +73,7 @@ async def kang(event):
             emoji = "🤔"
 
     packname = f"a{user.id}_by_{pack_username}_{number}{'_anim' if is_anim else ''}"
-    packtitle = (f"@{user.username or user.first_name}"
+    packtitle = (f"@{user.username or user.first_name}'s"
                 f"{number}{' animated' if is_anim else ''}")
     response = urllib.request.urlopen(
             urllib.request.Request(f'http://t.me/addstickers/{packname}'))
